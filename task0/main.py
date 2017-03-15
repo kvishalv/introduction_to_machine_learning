f2a24d523a62a07accbb889a254d76210e8f2aae
Nikolaos Korasidis <renelvon@gmail.com>
2017-03-15 15:27:24 +0100

    Format main properly

--- task0/main.py
+++ task0/main.py
@@ -2,14 +2,18 @@
 
 import numpy as np
 
-""" Data import """
-train = np.genfromtxt("data/train.csv", delimiter=',', skip_header=True)
-test = np.genfromtxt("data/test.csv", delimiter=',', skip_header=True)
-y_pred = np.mean(test[:, 1:], axis=1)
+def main():
+    """ Data import """
+    train = np.genfromtxt("data/train.csv", delimiter=',', skip_header=True)
+    test = np.genfromtxt("data/test.csv", delimiter=',', skip_header=True)
+    y_pred = np.mean(test[:, 1:], axis=1)
 
-result_test = np.column_stack((test[:, 0], y_pred))
-np.savetxt("test_result.csv", result_test, delimiter=",", fmt="%i, %1.4f", header="Id, y")
+    result_test = np.column_stack((test[:, 0], y_pred))
+    np.savetxt("test_result.csv", result_test, delimiter=",", fmt="%i, %1.4f", header="Id, y")
 
-# y = np.mean(train[:, 2:], axis=1)
-# result_train = np.column_stack((train[:, 0], y))
-# np.savetxt("train_result.csv", result_train, delimiter=",", fmt="%i, %1.1f", header="Id, y")
+#   y = np.mean(train[:, 2:], axis=1)
+#   result_train = np.column_stack((train[:, 0], y))
+#   np.savetxt("train_result.csv", result_train, delimiter=",", fmt="%i, %1.1f", header="Id, y")
+
+if __name__ == '__main__':
+    main()
