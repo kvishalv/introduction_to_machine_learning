44f6d755cabbe9df3c0f91d64434bb852a3cc5ab
Nikolaos Korasidis <renelvon@gmail.com>
2017-03-24 18:39:09 +0100

    Remove toy mean learners

--- task1/modules/Learners.py
+++ task1/modules/Learners.py
@@ -4,24 +4,6 @@ from scipy import linalg as splin
 from modules.AbstractLearner import AbstractLearner
 
 
-class MeanLearner(AbstractLearner):
-
-    def _train(self):
-        self._model = np.mean
-
-
-class AdvMeanLearner(AbstractLearner):
-    """ Sorts the training data, then takes the mean """
-
-    def _train(self):
-        self._model = self._mymean
-
-    @staticmethod
-    def _mymean(v):
-        v.sort()
-        return np.mean(v)
-
-
 class LeastSquaresLearner(AbstractLearner):
 
     def _train(self):
