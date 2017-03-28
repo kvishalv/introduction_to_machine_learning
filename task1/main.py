f68962784fc628746da369303ec2db199a80cada
Ozgen Eren <ozgeneren_@hotmail.com>
2017-03-28 23:47:57 +0200

    better performance indicators and small changes in find optimum alpha

--- task1/main.py
+++ task1/main.py
@@ -8,22 +8,36 @@ import warnings
 
 warnings.simplefilter("ignore")
 OUTPUT  = True
-learner = Model0()
+#learner = Model0()
+#learner = PolyLassoRegressionLearner()
+learner = SelectKBestLearner()
 
+"""
+train_size = 0.9
+PolyRidgeRegressionLearner: 1.40300900669e-07, alpha 100, poly 2, score: 25.3610838203
+PolyRidgeRegressionLearner: 1.40301129493e-08, alpha 10, poly 2, score: 25.0035653032
+PolyRidgeRegressionLearner: 1.91697708794e-07, alpha 100, poly 3, score: 41.0132351669
+Model0: 3.82868235438e-06, alpha 2000, poly 3, %90, score: 19.4278482076
+Model0: 1.61280520677e-06, alpha 800, poly 3, %98, score: 17.4999891433
+
+generalize optimum finder and its reporting
+add distributions for all x values as well and their correlations
+"""
 
 def main():
     """ Get the training data """
     train_set = CSVDataSet.from_train_data('data/train.csv', dtype=np.double)
-    train_set, validation_set = train_set.split(train_size=0.90)
+    train_set, validation_set = train_set.split(train_size=0.95)
 
     """ Train """
     learner.learn_from(train_set)
     print("Training error for", learner.__class__.__name__, "is:", learner.train_error)
-    learner.findOptimalAlpha(validation_set)
+    #learner.findOptimalAlpha(validation_set)
 
     """ Validate """
     verror = learner.validate_against(validation_set)
     print("Validation error for", learner.__class__.__name__, "is:", verror)
+    print("Validation error - training error:", learner.__class__.__name__, "is:", verror-learner.train_error)
 
     """ Get the test data, process & write the output """
     if OUTPUT:
--- task1/modules/Learners.py
+++ task1/modules/Learners.py
@@ -55,7 +55,7 @@ class RidgeRegressionLearner(SciKitLearner):
         x    = self._train_set.features
         y    = self._train_set.outputs
         clf = linear_model.Ridge(
-            alpha=0.1,
+            alpha=10,
             fit_intercept=True,
             random_state=42
         )
@@ -69,24 +69,37 @@ class PolyRidgeRegressionLearner(TransformingSciKitLearner):
         x    = self._train_set.features
         y    = self._train_set.outputs
 
-        self._transform = preprocessing.PolynomialFeatures(2)
+        self._transform = preprocessing.PolynomialFeatures(3)
 
         clf = linear_model.Ridge(
-            alpha=1.0,
+            alpha=100.0,
             fit_intercept=True,
         )
         clf.fit(self._transform.fit_transform(x), y)
         self._model = clf.predict
 
 
-class LassoRegressionLearner(TransformingSciKitLearner):
+class LassoRegressionLearner(SciKitLearner):
     def _train(self):
         x    = self._train_set.features
         y    = self._train_set.outputs
 
-        clf = linear_model.Lasso(1, max_iter=1e8)
+        clf = linear_model.Lasso(alpha = 100, max_iter=1e8)
+        clf.fit(x, y)
+        self._model = clf.predict
+
+
+class PolyLassoRegressionLearner(TransformingSciKitLearner):
+    def _train(self):
+        x    = self._train_set.features
+        y    = self._train_set.outputs
+
+        self._transform = preprocessing.PolynomialFeatures(3)
+
+        clf = linear_model.Lasso(alpha=0.4, max_iter=1e8)
         clf.fit(self._transform.fit_transform(x), y)
         self._model = clf.predict
+
 
 class ElasticNetLearner(SciKitLearner):
     def _train(self):
@@ -132,7 +145,7 @@ class Model0(TransformingSciKitLearner):
             #('kselect', feature_selection.SelectKBest(feature_selection.f_regression, k=750)),
         ])
 
-        clf = linear_model.Ridge(alpha=800, fit_intercept=True,)
+        clf = linear_model.Ridge(alpha=500, fit_intercept=True,)
         clf.fit(self._transform.fit_transform(x, y), y)
         self._model = clf.predict
 
@@ -145,22 +158,26 @@ class Model0(TransformingSciKitLearner):
             ('pselect', feature_selection.SelectPercentile(feature_selection.f_regression, percentile=98)),
         ])
 
-        nrIterations = 10
-        start        = 1
-        increment    = 100
+        nrIterations = 200
+        start        = 0.1
+        increment    = 10
 
-        rms = np.zeros(nrIterations)
+        tr_err  = np.zeros(nrIterations)
+        val_err = np.zeros(nrIterations)
         alp = np.zeros(nrIterations)
-        for i in range(0, nrIterations-1):
+        for i in range(0, nrIterations):
             alp[i] = (i*increment+start)
             clf = linear_model.Ridge(alpha=alp[i], fit_intercept=True,)
+            #clf = linear_model.Ridge(alpha=500, fit_intercept=True, )
             clf.fit(transformer.fit_transform(x, y), y)
             predictions = clf.predict(transformer.transform(validation_set.features))
-            rms[i] = self.rms_error(predictions, validation_set.outputs)
-        plt.plot(alp, rms)
-        plt.title('alpha values vs error')
+            val_err[i] = self.rms_error(predictions, validation_set.outputs)
+            predictions = clf.predict(transformer.transform(x))
+            tr_err[i]  = self.rms_error(predictions, y)
+        plt.plot(alp, val_err, 'r', alp, tr_err, 'b', alp, val_err-tr_err, 'g--')
+        plt.title('alpha values vs errors')
         plt.show()
-        print(rms, "\n", alp)
+        print(val_err, "\n", tr_err, "\n", alp)
 
 
 class SelectKBestLearner(TransformingSciKitLearner):
@@ -171,6 +188,6 @@ class SelectKBestLearner(TransformingSciKitLearner):
 
         self._transform = feature_selection.SelectKBest(score_func=feature_selection.f_regression, k=10)
 
-        clf = linear_model.Ridge(alpha=1.0, fit_intercept=True,)
+        clf = linear_model.Ridge(alpha=500, fit_intercept=True,)
         clf.fit(self._transform.fit_transform(x, y), y)
         self._model = clf.predict
