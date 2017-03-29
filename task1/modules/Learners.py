eb589a5f9582e547cadc3c2abc9b246465d22932
Nikolaos Korasidis <renelvon@gmail.com>
2017-03-29 21:26:31 +0200

    Remove simplistic NumPyLearners

--- task1/modules/Learners.py
+++ task1/modules/Learners.py
@@ -10,7 +10,6 @@ from sklearn import (
 )
 
 from modules.AbstractLearner import (
-    NumPyLearner,
     SciKitLearner,
     TransformingSciKitLearner
 )
@@ -26,29 +25,6 @@ NAMES = [
 'x10', 'x11', 'x12',
 'x13', 'x14', 'x15'
 ]
-
-
-class MoorePenroseLearner(NumPyLearner):
-
-    def _train(self):
-        a  = self._train_set.features
-        b  = self._train_set.outputs
-        at = np.linalg.pinv(a)
-        x  = at.dot(b)
-        self._model = lambda v: v.dot(x)
-
-
-class QRFactorizationLearner(NumPyLearner):
-
-    def _train(self):
-        a    = self._train_set.features
-        b    = self._train_set.outputs
-        q, r = np.linalg.qr(a)
-        # QRx = b; where Q is orthogonal and R is upper triangular
-        # Rx = Q^T b = (b^T Q)^T = b_proj
-        b_proj = b.transpose().dot(q).transpose()
-        x    = splin.solve_triangular(r, b_proj, overwrite_b=True)
-        self._model = lambda v: v.dot(x)
 
 
 class LinearRegressionLearner(SciKitLearner):
