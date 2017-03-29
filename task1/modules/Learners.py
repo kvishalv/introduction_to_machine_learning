7d289801837dd2e922c548e9ee3f47708e9eebaf
Nikolaos Korasidis <renelvon@gmail.com>
2017-03-29 20:41:55 +0200

    Add GridSearchCV Learner

--- task1/modules/Learners.py
+++ task1/modules/Learners.py
@@ -4,6 +4,7 @@ from sklearn import (
     feature_selection,
     ensemble,
     linear_model,
+    model_selection,
     pipeline,
     preprocessing,
 )
@@ -15,6 +16,16 @@ from modules.AbstractLearner import (
 )
 
 import matplotlib.pyplot as plt
+
+
+NAMES = [
+'y',
+'x1', 'x2', 'x3',
+'x4', 'x5', 'x6',
+'x7', 'x8', 'x9',
+'x10', 'x11', 'x12',
+'x13', 'x14', 'x15'
+]
 
 
 class MoorePenroseLearner(NumPyLearner):
@@ -266,12 +277,45 @@ class GridLearner(SciKitLearner):
         x    = self._train_set.features
         y    = self._train_set.outputs
 
-        self._transform = preprocessing.PolynomialFeatures(3)
+        #x, y = filter_outliers(x, y, n_estimators=200, contamination=0.01)
 
-        clf = linear_model.OrthogonalMatchingPursuit(n_nonzero_coefs=235, fit_intercept=True)
-        clf.fit(self._transform.fit_transform(x, y), y)
+        pipe = pipeline.Pipeline([
+            #('cosin', preprocessing.FunctionTransformer(add_sinus_cosinus)),
+        #    ('kselect', feature_selection.SelectKBest(feature_selection.f_regression, k=15)),
+            ('expand', preprocessing.PolynomialFeatures(include_bias=False)),
+            ('estim', linear_model.ElasticNet())
+        ])
 
-        self._model = clf.predict
+        param_grid = [{
+            'expand__degree': [3],
+            'estim': [linear_model.ElasticNet()],
+            'estim__fit_intercept': [True],
+            #'estim__alpha': [0.1, 0.2, 0.5, 1, 2, 5],
+            'estim__alpha': [0.325, 0.33, 0.335],
+            'estim__l1_ratio': [0.86, 0.865, 0.87],
+            #'lasso__alpha': list(0.315 + 0.001 * i for i in range(10)),
+        }]
+
+        grid = model_selection.GridSearchCV(
+            pipe, cv=6, n_jobs=4, param_grid=param_grid,
+            verbose=1
+        )
+        grid.fit(x, y)
+
+        print(grid.best_estimator_)
+
+        estim = grid.best_estimator_.named_steps['estim']
+        coeffs = estim.coef_
+
+        polyexp = grid.best_estimator_.named_steps['expand']
+        f_names = polyexp.get_feature_names(NAMES[1:])
+
+        """
+        for elem in sorted(zip(coeffs, f_names), reverse=True):
+            print(elem)
+        """
+
+        self._model = grid.predict
 
 
 class ARDRegressionLearner(TransformingSciKitLearner):
