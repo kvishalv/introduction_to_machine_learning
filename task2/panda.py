df140d5df2f23764ba2071afe34aa87e979ead40
Nikolaos Korasidis <renelvon@gmail.com>
2017-04-05 14:07:40 +0200

    Inspect data using pandas

---
+++ task2/panda.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.tools.plotting import scatter_matrix
+from sklearn import decomposition
+
+NAMES = [
+'y',
+'x1', 'x2', 'x3',
+'x4', 'x5', 'x6',
+'x7', 'x8', 'x9',
+'x10', 'x11', 'x12',
+'x13', 'x14', 'x15'
+]
+
+data = pd.read_csv('./data/train.csv', names=NAMES, header=1)
+datax = data.drop('y', axis=1)
+datay = data.drop(NAMES[1:], axis=1)
+
+datax.boxplot()
+
+for n in NAMES[1:]:
+    plt.figure()
+    x = data[n]
+    y = datay
+    plt.scatter(data[n], datay)
+    m,b = np.polyfit(x, y, 1) 
+    plt.plot(x, y, 'yo', x, m*x+b, '--k') 
+    plt.show()
+
+pca = decomposition.PCA(n_components=15)
+pca.fit(datax)
+print(pca.components_)
+ppp = pd.DataFrame(pca.components_)
+ppp.boxplot()
+plt.show()
