96c0ab0a336df50fed011509faf0233173113817
Nikolaos Korasidis <renelvon@gmail.com>
2017-04-07 01:00:38 +0200

    Drop outlier filtering for now

--- task2/modules/Learners.py
+++ task2/modules/Learners.py
@@ -12,31 +12,11 @@ from modules.AbstractLearner import (
 )
 
 
-NAMES = [
-'y',
-'x1', 'x2', 'x3',
-'x4', 'x5', 'x6',
-'x7', 'x8', 'x9',
-'x10', 'x11', 'x12',
-'x13', 'x14', 'x15'
-]
-
-
-def filter_outliers(x, y, **kwargs):
-    xy = np.column_stack((x,y))
-    filter_estimator = ensemble.IsolationForest(random_state=42, **kwargs)
-    filter_estimator.fit(xy)
-    is_inlier = filter_estimator.predict(xy)
-    return x[is_inlier == 1], y[is_inlier == 1]
-
-
 class NaiveBayesLearner(SciKitLearner):
 
     def _train(self):
         x    = self._train_set.features
         y    = self._train_set.outputs
-
-        #x, y = filter_outliers(x, y, n_estimators=200, contamination=0.003)
 
         pipe = pipeline.Pipeline([
             ('scale', preprocessing.StandardScaler()),
